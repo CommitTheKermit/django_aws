458f4dc7bc8fbdd1570898c0a9d28f0443241624
CommitTheKermit <qnf323@naver.com>
2023-05-30 23:53:40 +0900

    view adjust

--- oasis_venv/project_oasis/app_oasis/views.py
+++ oasis_venv/project_oasis/app_oasis/views.py
@@ -116,8 +116,8 @@ class EmailSendView(View):
         code = random.sample(range(10), 6)
         code = ''.join(map(str,code))
         try:
-            existFlag = EmailCode.objects.filter(uesr_email = data['user_email']).exists()
-            if not existFlag:
+            
+            if not EmailCode.objects.filter(uesr_email = data['user_email']).exists():
                 EmailCode(
                     user_email    = data['user_email'],
                     user_code     = code
@@ -127,7 +127,7 @@ class EmailSendView(View):
                     return JsonResponse({'message':"mail sent successfully"}, status=200)
                 except:
                     return JsonResponse({'message' : "MAIL ERROR"},status =400) 
-            elif existFlag:
+            elif EmailCode.objects.filter(uesr_email = data['user_email']).exists():
                 email_code = EmailCode.objects.get(user_email = data["user_email"])
                 email_code.delete()
                 try:
